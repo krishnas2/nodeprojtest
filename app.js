155692980f375267a30130a07204661f8ce59338
Krishna Kanth Juluru <krishnakanth.juluru@ad.infosys.com>
2020-08-10 00:01:36 +1000

    firstcommit

---
+++ app.js
@@ -0,0 +1,9 @@
+var express=require('express');
+var app=express();
+const PORT = process.env.PORT || 5000
+app.get('/',(req,res)=>{
+res.send('Hello World!');
+});
+var server=app.listen(PORT,()=>{
+	console.info(`Listening on ${ PORT }`);
+});
